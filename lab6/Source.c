ac797c243d90cecbd6a45a4a6b0e751d266b3e79
LyubachevVlad <113114501+LyubachevVlad@users.noreply.github.com>
2022-10-11 17:33:38 +0300

    Add files via upload

--- lab6/Source.c
+++ lab6/Source.c
@@ -21,32 +21,32 @@ void main(void)
 	(x <= 7) ? printf("%f\n", F = 3 * x - 9) : printf("%f\n", F = 1 / (pow(x, 2) - 4));
 	int a, b, c;
 	scanf("%d%d%d", &a, &b, &c);
-	if ((a>b)&&((a && b) > c))
+	if ((a>b)&&(a > c)&&(b>c))
 	{
 		printf("%d", s = a * 100 + b * 10 + c);
 	}
 	else
-	if ((a > c)&&((a&&c) > b))
+	if ((a > c)&&(a>b)&&(b<c))
 	{
 		printf("%d", s = a * 100 + c * 10 + b);
 	}
 	else
-	if ((b > a)&&((b&&a) > c))
+	if ((b > a)&&(b>c)&&(a>c))
 	{
 		printf("%d", s = b * 100 + a * 10 + c);
 	}
 	else
-	if ((b > c)&&((b&&c) > a))
+	if ((b > c)&&(c>a)&&(b>a))
 	{
 		printf("%d", s = b * 100 + c * 10 + a);
 	}
 	else
-	if ((c > a)&&((a&&c) > b))
+	if ((c > a)&&(b<c)&&(a>b))
 	{
 		printf("%d", s = c * 100 + a * 10 + b);
 	}
 	else
-	if ((c > b)&&((c&&b) > a))
+	if ((c > b)&&(b>a)&&(c>a))
 	{
 		printf("%d", s = c * 100 + b * 10 + a);
 	}
