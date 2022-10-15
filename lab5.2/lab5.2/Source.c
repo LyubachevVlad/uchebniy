5806f081bc5a4a81ac61b2798533fe1a8b469c8f
LyubachevVlad <113114501+LyubachevVlad@users.noreply.github.com>
2022-10-15 16:14:17 +0300

    Add files via upload

--- lab5.2/lab5.2/Source.c
+++ lab5.2/lab5.2/Source.c
@@ -23,11 +23,19 @@ float Y(float x)
 	float y = exp(2 * x) + pow(9.7, B(x));
 	return y;
 }
+float T(float p, float l, float i)
+{
+	float t = ((2 * cos(p - M_PI / 6)) / (0.5 + pow(sin(l), 2))) * (1 + (pow(i, 2) /(3 - pow(i, 2) / 5)));
+	return t;
+}
 void main(void)
 {
 	setlocale(LC_ALL, "RUS");
-	float rad, a, b, x, y;
-	int	C, Z, X;
+	float rad, a, b, x, y,p,l,i,k;
+	int	C, Z, X,m,n;
+	p = 14.26;
+	l = -1.22;
+	i = 3.5 * pow(10, -2);
 	puts("Введите число радиан");
 	scanf("%f", &x);
 	rad = R(x);
@@ -40,20 +48,10 @@ void main(void)
 	X = (int)b;
 	C = (int)y;
 	printf("\n целая часть от Z=%d, целая часть от X=%d, целая часть от C=%d\n", Z, X, C);
-	if (((Z % 2 == 0) && (X % 2 != 0)) || ((Z % 2 != 0) && (X % 2 == 0)))
-	{
-		puts("только одно из чисел Z и X четное: 1\n");
-	}
-	else
-	{
-		puts("только одно из чисел Z и X четное: 0\n");
-	}
-	if ((Z % 3 == 0) && (X % 3 == 0) && (C % 3 == 0))
-	{
-		puts("каждое из чисел Z,X,С кратно трем: 1\n");
-	}
-	else
-	{
-		puts("каждое из чисел Z,X,С кратно трем: 0\n");
-	}
+	n = (((Z % 2 == 0) && (X % 2 != 0)) || ((Z % 2 != 0) && (X % 2 == 0)));
+	printf("%d\n",n);
+	m = ((Z % 3 == 0) && (X % 3 == 0) && (C % 3 == 0));
+	printf("%d\n", m);
+	k = T(p, l, i);
+	printf("%f", k);
 }
