48dfb465a73ab3bc4abf0b56f9b354b8d0cf543e
LyubachevVlad <113114501+LyubachevVlad@users.noreply.github.com>
2022-12-15 16:27:13 +0300

    Add files via upload

--- lab16/lab16/Source.c
+++ lab16/lab16/Source.c
@@ -3,13 +3,19 @@
 #include<stdlib.h>
 double* mas(int size, double* ptr_array);
 double sum(int size, double* ptr_array);
-double delete_k(double* ptr_array, int size, int k);
-double find_element(double* ptr_array, int n, double element);
+double* delete(double* ptr_array, int size,double sr);
+double sredn(double* ptr_array, int size);
+double* mas2(int size2, double* ptr_array2);
+double* insert(double* ptr_array2, int* size2, int index, int num);
+int* pmin(double* ptr_array2, int* size2,double el);
 void main()
 {
 	setlocale(LC_ALL, "RUS");
 	double* ptr_array;
-	int size, k;
+	double* ptr_array2;
+	int size, k,size2;
+	double sr=0;
+	double num = -999;
 	srand(time(NULL));
 	printf("Введите размер массива: ");
 	scanf("%d", &size);
@@ -21,14 +27,35 @@ void main()
 	}
 	mas(size, ptr_array);
 	sum(size, ptr_array);
-	puts("\nКакой элемент массива удалить?");
-	scanf("%d", &k);
-	delete_k(ptr_array, size, k);
-	size--;
-	for (int i = 0; i < size; i++)
+	printf("\n\n\n");
+	double sredn(double* ptr_array, int size);
+	delete(ptr_array, size,sr);
+	printf("\n\n\n");
+	srand(time(NULL));
+	printf("Введите размер массива: ");
+	scanf("%d", &size2);
+	ptr_array2 = (double*)malloc(size2 * sizeof(double));
+	if (ptr_array2 == NULL)
 	{
-		printf("array[%d] = %lf\n", i, ptr_array[i]);
+		puts("error");
+		return -1;
 	}
+     mas2(size2, ptr_array2);
+	 int i = 0;
+	 double min = ptr_array2[i];
+	 for (int i = 0;i < size2;i++)
+	 {
+		 if (ptr_array2[i] < min)
+		 {
+			 min = ptr_array2[i];
+		 }
+	 }
+	 printf("\n");
+	 ptr_array2=insert( ptr_array2, &size2, pmin(ptr_array2, size2,min), -999);
+	 for (int i = 0; i < size2 ; i++)
+	 {
+		 printf("\nptr_array2[%d] = %lf", i, ptr_array2[i]);
+	 }
 }
 double* mas(int size, double* ptr_array)
 {
@@ -69,25 +96,67 @@ double sum(int size, double* ptr_array)
 	sr2 = pr / ot;
 	printf("  Среднеезанчение на промежутке=%lf", sr2);
 }
-double delete_k(double* ptr_array, int size, int k) 
+double sredn(double* ptr_array, int size)
 {
-	int n = size - 1;
-	for (int i = k; i < (size - 1); i++) 
+	double k = 0,sr=0;
+	for (int i = 0;i < size;i++)
 	{
-		for (int i = (find_element(ptr_array, size, k) - 1); 
-			i < (size - 1); i++) ptr_array[i] = ptr_array[i + 1];
+		k += ptr_array[i];
 	}
-	return n;
+	sr = k / size;
+	return sr;
 }
-double find_element(double* ptr_array, int n, double element)
+double* delete(double* ptr_array, int size,double sr) 
 {
-	int element1 = -1;
-	for (int i = 0; i < n; i++)
+	for (int i = 0;i < size;i++)
 	{
-		if (ptr_array[i] == element)
+		if (ptr_array[i] < sr)
 		{
-			element1 = i + 1;
+			for (int j = i;j < size;j++)
+				ptr_array[j] = ptr_array[j + 1];
+				size--;
+			}
+		}
+		for (int i = 0;i < size;i++)
+		{
+			printf("\nptr_array[%d] = %lf", i, ptr_array[i]);
+		}
+	return ptr_array;
+} 
+double* mas2(int size2, double* ptr_array2)
+{
+	for (int i = 0; i < size2; i++)
+	{
+		ptr_array2[i] = -100 + 1.f * (100 - (-100)) * rand() / RAND_MAX;
+	}
+	for (int i = 0; i < size2; i++)
+	{
+		printf("\nptr_array2[%d] = %lf", i, ptr_array2[i]);
+	}
+	return ptr_array2;
+}
+double* insert(double* ptr_array2, int* size2, int index, int num)
+{
+		int size_n = (*size2) + 1;
+		if (ptr_array2 == NULL) return NULL;
+		double*ptr_array_n = (double*)realloc(ptr_array2, size_n * sizeof(double));
+		if (ptr_array_n == NULL) return ptr_array2;
+		ptr_array2 = ptr_array_n;
+		for (int i = size_n - 1; i > index;i--)
+			ptr_array2[i] = ptr_array2[i - 1];
+		ptr_array2[index] = num;
+		*size2 = size_n;
+		return ptr_array2;
+}
+int* pmin(double* ptr_array2, int* size2,double el)
+{
+	int pos = -1;
+	for (int i = 0;i < size2;i++)
+	{
+		if (ptr_array2[i] == el)
+		{
+			pos = i;
 		}
 	}
-	return element1;
+	return pos;
 }
