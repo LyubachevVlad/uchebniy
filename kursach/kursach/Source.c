5355f8f794bd00d4f682ee6b087117429dae35f8
LyubachevVlad <113114501+LyubachevVlad@users.noreply.github.com>
2022-12-22 00:28:10 +0300

    Add files via upload

--- kursach/kursach/Source.c
+++ kursach/kursach/Source.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <locale.h>
 #include <math.h>
+#include <stdlib.h>
+#include <io.h>
+#include <string.h>
+typedef double (*Tfun)(double);
+struct bil
+{
+	double x;
+	double x1;
+	double x2;
+	double tab;
+	Tfun f;
+};
+typedef struct bil Bil;
 double Y(double x)
 {
 	double y;
 	y = exp(x) + 5 + cos(0.001 * x);
 	return y;
 }
-double F(double x)
+double V(double x)
 {
-	double f;
+	double f,v;
 	if (x >= 1)
 	{
 		f = x + x;
@@ -18,30 +31,40 @@ double F(double x)
 	{
 		f = pow(x, 3) + 0.2;
 	}
-	return f;
-}
-double V(double x)
-{
-	double v;
-	v = 2 * pow(cos(pow(x, 2)), 3) + pow(sin(pow(x, 3)), 2) - 0.08 * F(x);
+	v = 2 * pow(cos(pow(x, 2)), 3) + pow(sin(pow(x, 3)), 2) - 0.08 * f;
 	return v;
 }
-typedef double (*Tfun)(double);
 double Y(double);
-double F(double);
 double V(double);
 void i(Tfun f, double xn, double xk, double h)
 {
 	char u = ' ';
-	printf("\n|%2cx%2c|%4cF(x)%5c|",u,u,u,u);
+	printf("\n|%2cx%3c|%4cF(x)%5c|",u,u,u,u);
 	for (double x = xn; x <= xk; x += h)
 	{
-		printf("\n|%5.2lf|%13.6lf|", x, f(x));
+		printf("\n|%6.2lf|%13.6lf|", x, f(x));
 	}
+}
+void file(Tfun f, double xn, double xk, double h)
+{
+	FILE* tabl;
+	char name[20] = "tabl.txt";
+	if ((tabl=fopen(name,"wt"))==NULL)
+	{
+		printf("Ошибка открытия файла");
+		return 0;
+	}
+	char u = " ";
+	fprintf(tabl, "\n |%3cx%4c|%4cF(x)%7c|", u, u, u, u);
+	for (double x = xn; x <= xk; x += h)
+	{
+		fprintf(tabl,"\n | %6.2lf | %13.6lf |", x, f(x));
+	}
+	fclose(tabl);
 }
 void plot(double x0, double x1, Tfun f)
 {
-	int SCREENW = 40, SCREENH = 40;//размеры поля вывода в символах
+	int SCREENW = 40, SCREENH = 40;
 	char screen[40][40];
 	double x, y[40];
 	double ymin = 0, ymax = 0;
@@ -51,14 +74,13 @@ void plot(double x0, double x1, Tfun f)
 	hx = (x1 - x0) / (SCREENW - 1);
 	for (i = 0, x = x0; i < SCREENW; ++i, x += hx)
 	{
-		y[i] = f(x); //расчет значений функции для каждой точки поля вывода графика
+		y[i] = f(x); 
 		if (y[i] < ymin) ymin = y[i];
 		if (y[i] > ymax) ymax = y[i];
 	}
 	hy = (ymax - ymin) / (SCREENH - 1);
 	yz = (int)floor(ymax / hy + 0.5);
 	xz = (int)floor((0. - x0) / hx + 0.5);
-	//построение осей и заполнение массива отображения пробелами
 	for (j = 0; j < SCREENH; ++j)
 		for (i = 0; i < SCREENW; ++i)
 		{
@@ -67,72 +89,186 @@ void plot(double x0, double x1, Tfun f)
 			else if (i == xz) screen[i][j] = '|';
 			else screen[i][j] = ' ';
 		}
-	//определение положения значения функции на поле вывода
 	for (i = 0; i < SCREENW; ++i)
 	{
 		j = (int)floor((ymax - y[i]) / hy + 0.5);
 		screen[i][j] = '*';
 	}
-	//печать массива символов
 	for (j = 0; j < SCREENH; ++j)
 	{
 		for (i = 0; i < SCREENW; ++i)  putchar(screen[i][j]);
 		putchar('\n');
 	}
 }
+void shiftr(int arr[50][50], int s, int shift)
+{
+	for (int i = 0; i < s; i++)
+	{
+		int t0 = arr[i][0], t1 = 0;
+		for (int j = 0, k = 0; k < s; k++)
+		{
+			t1 = arr[i][(j + shift) % s];
+			arr[i][(j + shift) % s] = t0;
+			j = (j + shift) % s;
+			t0 = t1;
+		}
+	}
+	for (int i = 0; i < s; i++)
+	{
+		for (int j = 0; j < s; j++)
+		{
+			printf("%3d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+void shiftd(int arr[50][50], int s, int shift)
+{
+	for (int j = 0; j < s; j++)
+	{
+		int t0 = arr[0][j], t1 = 0;
+		for (int i = 0, k = 0; k < s; k++)
+		{
+			t1 = arr[(i + shift) % s][(j) % s];
+			arr[(i + shift) % s][j] = t0;
+			i = (i + shift) % s;
+			t0 = t1;
+		}
+	}
+	for (int i = 0; i < s; i++)
+	{
+		for (int j = 0; j < s; j++)
+		{
+			printf("%3d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
 void main()
 {
 	setlocale(LC_ALL, "RUS");
-	double x, x1, x2, tab;
-	char c = '0', sk1, sk2,f;
-	while (c != '6')
+	Bil lib = { 5,-1,1,1,Y };
+	char sk1, sk2,f;
+	int c=0, marker = 0, marker2 = 0;
+	while (c != 6)
 	{
+		puts("");
 		puts("\nКакое действие:\n 1.Найти значение функции Y(x)\n 2.Найти значение функции F(x)\n 3.Вычислить значения функции Y(x) на заданном интервале, полуинтервале или отрезке\n 4.Вычислить значения функции V(x) на заданном интервале полуинтервале или отрезке\n 5.Построить график, выбранной функции Y(x) и F(x)\n 6.Завершение работы ");
-		scanf("%c", &c);
+		scanf("%d", &c);
 		switch (c)
 		{
-		case'1':
+		case 1:
 			puts("\nВведите переменную x:");
-			scanf("%lf", &x);
-			printf("\n%lf", Y(x));
+			scanf("%lf", &lib.x);
+			printf("\n%lf", lib.f(lib.x));
 			break;
-		case'2':
+		case 2:
 			puts("\nВведите переменную x:");
-			scanf("%lf", &x);
-			printf("%lf\n", V(x));
+			scanf("%lf", &lib.x);
+			lib.f = V;
+			printf("%lf\n", lib.f(lib.x));
 			break;
-		case'3':
-			puts("\nВведите интервал:");
-			scanf(" %c%lf%lf%c", &sk1 ,&x1, &x2,&sk2);
-			puts("\nВведите шаг табуляции:");
-			scanf(" %lf", &tab);
-			if (sk1 == '(') x1 += tab;
-			if (sk2 == ')') x2 -= tab;
-			i(Y, x1, x2, tab);
+		case 3:
+			printf("Выводить значения в консоль-1,выводить значения в файл-2\n");
+			scanf("%d", &marker);
+			switch (marker)
+			{
+			case 1:
+				puts("\nВведите интервал:");
+				scanf(" %c%lf%lf%c", &sk1, &lib.x1, &lib.x2, &sk2);
+				puts("\nВведите шаг табуляции:");
+				scanf(" %lf", &lib.tab);
+				if (sk1 == '(') lib.x1 += lib.tab;
+				if (sk2 == ')') lib.x2 -= lib.tab;
+				i(lib.f, lib.x1, lib.x2, lib.tab);
+				break;
+			case 2:
+				puts("\nВведите интервал:");
+				scanf(" %c%lf%lf%c", &sk1, &lib.x1, &lib.x2, &sk2);
+				puts("\nВведите шаг табуляции:");
+				scanf(" %lf", &lib.tab);
+				if (sk1 == '(') lib.x1 += lib.tab;
+				if (sk2 == ')') lib.x2 -= lib.tab;
+				file(lib.f, lib.x1, lib.x2, lib.tab);
+				break;
+			}
 			break;
-		case'4':
-			puts("\nВведите интервал:");
-			scanf(" %c%lf%lf%c", &sk1, &x1, &x2, &sk2);
-			puts("\nВведите шаг табуляции:");
-			scanf("%lf", &tab);
-			if (sk1 == '(') x1 += tab;
-			if (sk2 == ')') x2 -= tab;
-			i(V, x1, x2, tab);
+		case 4:
+			printf("Выводить значения в консоль-1,выводить значения в файл-2\n");
+			scanf("%d", &marker2);
+			switch (marker2)
+			{
+			case 1:
+				puts("\nВведите интервал:");
+				scanf(" %c%lf%lf%c", &sk1, &lib.x1, &lib.x2, &sk2);
+				puts("\nВведите шаг табуляции:");
+				scanf(" %lf", &lib.tab);
+				if (sk1 == '(') lib.x1 += lib.tab;
+				if (sk2 == ')') lib.x2 -= lib.tab;
+				i(lib.f, lib.x1, lib.x2, lib.tab);
+				break;
+			case 2:
+				puts("\nВведите интервал:");
+				scanf(" %c%lf%lf%c", &sk1, &lib.x1, &lib.x2, &sk2);
+				puts("\nВведите шаг табуляции:");
+				scanf(" %lf", &lib.tab);
+				if (sk1 == '(') lib.x1 += lib.tab;
+				if (sk2 == ')') lib.x2 -= lib.tab;
+				file(lib.f, lib.x1, lib.x2, lib.tab);
+				break;
+			}
 			break;
-		case'5':
+		case 5:
 			puts("\nВведите интервал:");
-			scanf(" %c%lf%lf%c", &sk1, &x1, &x2, &sk2);
+			scanf(" %c%lf%lf%c", &sk1, &lib.x1, &lib.x2, &sk2);
 			puts("\nВведите шаг табуляции:");
-			scanf(" %lf", &tab);
+			scanf(" %lf", &lib.tab);
 			puts("Какую функцию нарисовать?");
 			scanf(" %c", &f);
-			if (f=='y') plot(x1, x2, Y);
-			if (f=='v') plot(x1, x2, V);
+			if (f=='y') plot(lib.x1, lib.x2, lib.f);
+			if (f=='v') plot(lib.x1, lib.x2, lib.f);
 			break;
 		default:
-			puts("Неопознаный символ");
+			puts("Завершение");
 			break;
 		}
-		break;
+	}
+	puts("");
+	int arr[50][50],s;
+	int flag=0,shift;
+	srand(time(NULL));
+	printf("Введите размерность:");
+	scanf("%d", &s);
+	for (int i = 0; i < s; i++)
+	{
+		for (int j = 0; j < s; j++)
+		{
+			arr[i][j] = -11 + 1. * (11 - (-11)) * rand() / RAND_MAX;
+			printf("%3d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	puts("");
+	while (flag != 3)
+	{
+		printf("\nКакой сдвиг хотите получить?\n");
+		printf("1-в право,2-вниз,3-завершение\n");
+		scanf("%d", &flag);
+		switch (flag)
+		{
+		case 1:
+			printf("Введите желаемый сдвиг\n");
+			scanf("%3d", &shift);
+			shiftr(arr, s, shift);
+			break;
+		case 2:
+			printf("Введите желаемый сдвиг\n");
+			scanf("%3d", &shift);
+			shiftd(arr, s, shift);
+			break;
+		default:
+			puts("Завершение");
+			break;
+		}
 	}
 }
